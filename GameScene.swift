26ac5e3294e68033f2929affbea19a1d5303adab
Todd Halkowski <thalkowski@virtusa.com>
2015-09-04 14:08:40 -0500

    Flappy Bird 2
    added physicsBody for gravity and impulse for bird, added ground so
    bird would bounce and not fall through screen

---
+++ GameScene.swift
@@ -0,0 +1,79 @@
+//
+//  GameScene.swift
+//  Flappy Bird
+//
+//  Created by Todd Halkowski
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    var bird = SKSpriteNode()
+    var bg = SKSpriteNode()
+    
+    override func didMoveToView(view: SKView) {
+        
+        var bgTexture = SKTexture(imageNamed: "img/bg.png")
+        
+        
+        var movebg = SKAction.moveByX(-bgTexture.size().width, y: 0, duration: 9)
+        var replacebg = SKAction.moveByX(bgTexture.size().width, y: 0, duration: 0)
+        var movebgForever = SKAction.repeatActionForever(SKAction.sequence([movebg, replacebg]))
+        
+        for var i:CGFloat=0; i<3; i++ {
+            
+            bg = SKSpriteNode(texture: bgTexture)
+            bg.position = CGPoint(x: bgTexture.size().width/2 + bgTexture.size().width * i, y: CGRectGetMidY(self.frame))
+            bg.size.height = self.frame.height
+            
+            
+            bg.runAction(movebgForever)
+            
+            
+            self.addChild(bg)
+            
+            
+            }
+        
+        
+        
+        
+        var birdTexture = SKTexture(imageNamed: "img/flappy1.png")
+        var birdTexture2 = SKTexture(imageNamed: "img/flappy2.png")
+        
+        var animation = SKAction.animateWithTextures([birdTexture, birdTexture2], timePerFrame: 0.1)
+        var makeBirdFlap = SKAction.repeatActionForever(animation)
+        
+        bird = SKSpriteNode(texture: birdTexture)
+        bird.position = CGPoint(x: CGRectGetMidX(self.frame), y: CGRectGetMidY(self.frame))
+        bird.runAction(makeBirdFlap)
+        
+        bird.physicsBody = SKPhysicsBody(circleOfRadius: bird.size.height / 2)
+        bird.physicsBody?.dynamic = true
+        bird.physicsBody?.allowsRotation = false
+        
+        
+        self.addChild(bird)
+        
+        var ground = SKNode()
+        ground.position = CGPointMake(0, 0)
+        ground.physicsBody = SKPhysicsBody(rectangleOfSize: CGSizeMake(self.frame.size.width, 1))
+        ground.physicsBody?.dynamic = false
+        
+        self.addChild(ground)
+        
+    }
+    // UPDATE — Changed "(touches: NSSet, withEvent event: UIEvent)" to "(touches: Set<NSObject>, withEvent event: UIEvent)"
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        /* Called when a touch begins */
+        
+        bird.physicsBody?.velocity = CGVectorMake(0, 0)
+        bird.physicsBody?.applyImpulse(CGVectorMake(0, 50))
+       
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
